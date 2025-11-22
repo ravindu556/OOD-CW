c2d86a09cc9b5bd160db669704019194d67c129c
Ravindu <ravindu.20241027@iit.ac.lk>
2025-11-22 09:33:41 -0800

    commit

---
+++ src/teammate/Main.java
@@ -0,0 +1,13 @@
+package teammate;
+
+import java.util.Scanner;
+
+ public class Main {
+     private static final Scanner scanner = new Scanner(System.in);
+
+     public static void main(String[] args) {
+         System.out.println("==================================================");
+         System.out.println("            TEAM MATE SYSTEM");
+         System.out.println("==================================================\n");
+     }
+ }
