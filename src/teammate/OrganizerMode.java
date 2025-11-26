e098c6c086ec859b98389ba90cd1e1d4c895e2d0
Ravindu <ravindu.20241027@iit.ac.lk>
2025-11-25 18:02:08 -0800

    commit

---
+++ src/teammate/OrganizerMode.java
@@ -0,0 +1,11 @@
+package teammate;
+
+import java.util.*;
+
+public class OrganizerMode {
+
+    private static final Scanner sc = new Scanner(System.in);
+    private static List<Participant> participants;
+    private static List<Team> formedTeams = new ArrayList<>();
+
+}
